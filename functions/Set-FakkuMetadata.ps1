45055af8eb2a174bd46a02fde50d8305dc80b05b
jvlflame <jli141928@gmail.com>
2019-10-06 13:02:19 -0700

    Add full functionality to add metadata to archive

--- functions/Set-FakkuMetadata.ps1
+++ functions/Set-FakkuMetadata.ps1
@@ -3,19 +3,35 @@ function Set-FakkuMetadata {
         param(
                 [Parameter(Mandatory = $true)]
                 [System.IO.FileInfo]$FilePath,
-                [Parameter(Mandatory = $true)]
+                [Parameter(Mandatory = $false)]
                 [Switch]$Recurse
         )
 
         
         foreach ($File in (Get-LocalFakkuFiles -FilePath $FilePath -Recurse:$Recurse)) {
-                $FakkuUrl = Get-FakkuURL -DoujinName $File.BaseName
-                try {
+                $FakkuUrl = Get-FakkuURL -DoujinName $File.Name
+                Try {
                         $WebRequest = Invoke-WebRequest -Uri $FakkuUrl -Method Get
-                        Write-FakkuXML -WebRequest $WebRequest -XMLPath (Join-Path -Path $FilePath -ChildPath 'ComicInfo.xml')
+                        $XMLPath = Join-Path -Path $FilePath -ChildPath 'ComicInfo.xml'
+                        Write-Host "XML Path: $XMLPath"
+                        Write-Host "Destination Path: $DestinationPath"
+                        Write-FakkuXML -WebRequest $WebRequest.Content -XMLPath $XMLPath
+
+                        # Temporarily rename files to bypass PowerShell path errors 
+                        # that occur when path contains square brackets "[]"
+                        if ($File.Name -match "\[" -or $File.Name -match "\]") {
+                                $NewName = ($File.FullName -replace "\[", "_") -replace "\]", "_"
+                                Rename-Item -LiteralPath $File -NewName $NewName
+                        }
+
+                        Compress-Archive -Path $XMLPath -Update -DestinationPath $NewName
+                        Remove-Item -Path $XMLPath -Force
+                        Rename-Item -LiteralPath $NewName -NewName $File.Name
                 }
-                catch {
-                        Write-Error ""$File.BaseName" not found on Fakku"
+
+                Catch [Microsoft.PowerShell.Commands.HttpResponseException] {
+                        $DoujinName = $File.BaseName
+                        Write-Warning "Could not find '$DoujinName' on Fakku. Check filename to make sure it follows the approved schema."
                 }
         }
 }
