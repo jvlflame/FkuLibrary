7e81101db1506cf82400eff04b2de15734f84fc5
jvlflame <jli141928@gmail.com>
2019-10-06 14:44:49 -0700

    Add match for alternate file naming

--- internal/Get-FakkuUrl.ps1
+++ internal/Get-FakkuUrl.ps1
@@ -7,10 +7,20 @@ function Get-FakkuURL {
         
         # Gets the filename and converts it to a parseable Fakku web URL
         # The filename must match exactly what is presented by Fakku or scrape will fail
-        $CleanFileName = ((((($DoujinName -split "]")[1])`
-                                        -split "\(")[0]).Trim()) -replace " ", "-"
-        
-        
+        # Match and clean "[Artist] FileName.zip"
+        if ($DoujinName -match '^\[(.*?)\]') {
+                $CleanFileName = (((((($DoujinName -split "]")[1])`
+                                                        -split "\(")[0]).Trim())`
+                                -replace '[^-a-z0-9 ]+', '')`
+                        -replace ' ', '-'
+        }
+
+        # Match and clean "FileName.zip"
+        elseif ($DoujinName -match '^[a-zA-Z0-9]') {
+                $CleanFileName = ($DoujinName -replace '[^-a-z0-9 ]+', '')`
+                        -replace ' ', '-'
+        }
+                
         $FakkuUrl = "https://www.fakku.net/hentai/$CleanFileName-english"
         Write-Output $FakkuUrl
 }
