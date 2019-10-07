bc3b4bff3ff06d6db513b431fb1fb37609e288e8
jvlflame <jli141928@gmail.com>
2019-10-06 17:03:55 -0700

    Set XML archive to function

--- functions/Set-FakkuMetadata.ps1
+++ functions/Set-FakkuMetadata.ps1
@@ -9,26 +9,21 @@ function Set-FakkuMetadata {
 
         foreach ($File in (Get-LocalFakkuFiles -FilePath $FilePath -Recurse:$Recurse)) {
                 $FakkuUrl = Get-FakkuURL -DoujinName $File.BaseName
+                $DoujinName = $File.BaseName
                 Try {
                         $WebRequest = Invoke-WebRequest -Uri $FakkuUrl -Method Get
                         $XMLPath = Join-Path -Path $FilePath -ChildPath 'ComicInfo.xml'
                         Write-FakkuXML -WebRequest $WebRequest.Content -XMLPath $XMLPath
-
-                        # Temporarily rename files to bypass PowerShell path errors 
-                        # that occur when path contains square brackets "[]"
-                        if ($File.Name -match "\[" -or $File.Name -match "\]") {
-                                $NewName = ($File.FullName -replace "\[", "_") -replace "\]", "_"
-                                Rename-Item -LiteralPath $File -NewName $NewName
-                        }
-
-                        Compress-Archive -Path $XMLPath -Update -DestinationPath $NewName
-                        Remove-Item -Path $XMLPath -Force
-                        Rename-Item -LiteralPath $NewName -NewName $File.Name
+                        Set-MetadataXML -FilePath $File.Name -XMLPath $XMLPath
                 }
 
                 Catch [Microsoft.PowerShell.Commands.HttpResponseException] {
-                        $DoujinName = $File.BaseName
-                        Write-Warning "Could not find '$DoujinName' on Fakku. Check filename to make sure it follows the approved schema."
+                        Write-Warning "Could not find '$DoujinName' on Fakku. Manga may not exist on Fakku, the filename does not follow the approved schemas, or may be inaccessible without authentication."
+                        Get-PandaChaikaMetadata -DoujinName $DoujinName
+                }
+
+                Catch [System.Net.WebException] {
+                        Write-Warning "Could not access '$DoujinName' on Fakku. Manga is inaccessible without authentication."
                 }
 
                 Start-Sleep -Seconds 1
---
+++ internal/Set-MetadataXML.ps1
@@ -0,0 +1,20 @@
+function Set-MetadataXML {
+        [CmdletBinding()]
+        param(
+                [Parameter(Mandatory = $true)]
+                [String]$FilePath,
+                [Parameter(Mandatory = $true)]
+                [String]$XMLPath
+        )
+
+        # Temporarily rename files to bypass PowerShell path errors 
+        # that occur when path contains square brackets "[]"
+        if ($File.Name -match "\[" -or $File.Name -match "\]") {
+                $NewName = ($File.FullName -replace "\[", "_") -replace "\]", "_"
+                Rename-Item -LiteralPath $File -NewName $NewName
+        }
+
+        Compress-Archive -Path $XMLPath -Update -DestinationPath $NewName
+        Remove-Item -Path $XMLPath -Force
+        Rename-Item -LiteralPath $NewName -NewName $FilePath
+}
