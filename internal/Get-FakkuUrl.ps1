c671fcdc3f3f61020b6a06b7ac81033dc406071f
jvlflame <jeffvictorli@gmail.com>
2021-04-05 13:37:44 -0700

    Remove dash replacement

--- internal/Get-FakkuUrl.ps1
+++ internal/Get-FakkuUrl.ps1
@@ -6,7 +6,6 @@ function Get-FakkuURL {
         )
         $DoujinName = $DoujinName -replace '★', 'bzb' `
                 -replace '&', '' `
-                -replace '-', '' `
                 -replace '  ', ' '
         # Gets the filename and converts it to a parseable Fakku web URL
         # The filename must match exactly what is presented by Fakku or scrape will fail
