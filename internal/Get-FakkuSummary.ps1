b610747ad23013230ef9c8ab779abcbaa5c19303
shrublet <63182228+shrublet@users.noreply.github.com>
2021-11-29 07:28:13 -0800

    Revert newline insertion
    Wasn't reliable and was solving a niche edge-case scenario anyways. Too often inserts new lines after ellipses where they're not intended.

--- internal/Get-FakkuSummary.ps1
+++ internal/Get-FakkuSummary.ps1
@@ -5,10 +5,8 @@ function Get-FakkuSummary {
                 [String]$WebRequest
         )
 
-        # Try to detect where line breaks should be and insert them (letter/number directly following punctuation)
         $Summary = (((($WebRequest -split '<meta name="description" content="')[1])`
                 -split '">')[0]).Trim()`
-                -replace '(\.|\?|\!)([a-zA-Z0-9])', ('$1'+"`n`n"+'$2')`
                 -replace '&(?!amp;)', '&amp;'
 
         # Removed since description is grabbed from header now
