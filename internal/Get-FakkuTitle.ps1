37c0ff3e50c972dd6a40fde141b5850c12bedf60
shrublet <63182228+shrublet@users.noreply.github.com>
2021-11-28 17:31:22 -0800

    Fix ampersand replacement

--- internal/Get-FakkuTitle.ps1
+++ internal/Get-FakkuTitle.ps1
@@ -7,7 +7,7 @@ function Get-FakkuTitle {
 
         # Site layout change makes it easier to grab title from <title> tag
         $Title = (($WebRequest -split '<title>(.*?)[Hh]entai by.*<\/title>')[1]).Trim()`
-                -replace '&', '&amp;'
+                -replace '&(?!amp;)', '&amp;'
 
         Write-Output $Title
 }
